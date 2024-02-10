43ccb81ee73da5aeae502a638bd449dff7b25f31
Merylly <142231027+Merylly@users.noreply.github.com>
2024-02-10 11:07:49 +0200

    d

---
+++ src/main.js
@@ -0,0 +1,145 @@
+const API = 'https://energyflow.b.goit.study/api';
+
+// function quote(date) {
+//     return fetch('${API}')
+// }
+
+function quote() {
+  //   const urlParams = new URLSearchParams({
+  //     key: API_KEY,
+  //     q: picture,
+  //     image_type: 'photo',
+  //     orientation: 'horizontal',
+  //     safesearch: true,
+  //     per_page: 15,
+  //   });
+
+  return fetch(`${API}/quote`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
+}
+
+console.log(quote());
+
+const dateNow = new Date;
+
+
+console.log(dateNow);
+
+localStorage.setItem('date', dateNow);
+
+
+
+import axios from 'axios';
+
+const filterButtons = document.querySelector('.filter-buttons');
+const exerciseFiltersList = document.querySelector('.exercise-filters-list');
+const pagination = document.querySelector('.pagination');
+const BASE_URL = 'https://energyflow.b.goit.study/api';
+let filterValueDefault = 'Muscles';
+let currentPage = 1;
+let screenWidth = window.innerWidth;
+let currentLimit = 0;
+
+filterButtons.addEventListener('click', filterBtnClick);
+
+if (screenWidth <= 375) {
+  currentLimit = 8;
+} else if (screenWidth <= 768) {
+  currentLimit = 12;
+} else {
+  currentLimit = 12;
+}
+
+async function getExercises() {
+  try {
+    const response = await axios.get(`${BASE_URL}/filters`, {
+      params: {
+        filter: filterValueDefault,
+        page: currentPage,
+        limit: currentLimit,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function filterBtnClick(event) {
+  event.preventDefault();
+  currentPage = 1;
+  const filterValue = event.target;
+  const qwer = filterValue.dataset.filter;
+  filterValueDefault = qwer;
+  exerciseFiltersList.innerHTML = '';
+  console.log(qwer);
+  if (event.target.tagName !== 'BUTTON') {
+    return;
+  }
+  try {
+    getExercises(qwer).then(data => {
+      const { results, totalPages, page } = data;
+      markupExercises(results);
+      if (totalPages > 1) {
+        const pag = paginationPages(page, totalPages);
+        pagination.innerHTML = pag;
+      } else {
+        pagination.innerHTML = '';
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function markupExercises(results) {
+  const markup = results
+    .map(
+      ({ name, filter, imgUrl }) => ` <li class="filter-list" data-filter>
+        <img class="img-exercises" src="${imgUrl}" alt="${filter}">
+        <div class="filter-text">
+          <p class="filter-exercises">${name}</p>
+          <p class="filter-name">${filter}</p>
+        </div>
+      </li>`
+    )
+    .join('');
+  exerciseFiltersList.insertAdjacentHTML('beforeend', markup);
+}
+
+function paginationPages(page, totalPages) {
+  let paginationHtml = '';
+  for (let i = 1; i <= totalPages; i += 1) {
+    paginationHtml += `<button class="pagination-btn" type="button">${i}</button>`;
+  }
+  return paginationHtml;
+}
+
+async function onPaginationPages(e) {
+  console.log(e.target.textContent);
+  console.log(currentPage);
+  currentPage = e.target.textContent;
+  exerciseFiltersList.innerHTML = '';
+  try {
+    const { results, page, totalPages } = await getExercises();
+    console.log(results);
+    console.log(page);
+    console.log(totalPages);
+    const filter = results[0].filter;
+    console.log(filter);
+
+    if (page === totalPages) {
+      return;
+    }
+    markupExercises(results);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+pagination.addEventListener('click', onPaginationPages);
